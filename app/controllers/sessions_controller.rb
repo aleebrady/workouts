9ea5f444058f6d43314cd46c42ad4585d14f809a
aleebrady <aleebrady@gmail.com>
2020-10-13 12:07:14 -0700

    added login form_for

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,27 @@
 class SessionsController < ApplicationController
 
     def home
+
     end
 
     def new
+        @user = User.new
+        render :login
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to workouts_path
+        else
+            render :login
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to '/'
     end
 
 end
