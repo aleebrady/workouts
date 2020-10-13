b4cb30cd63a9078e7a33f731f106a0aa96dece02
aleebrady <aleebrady@gmail.com>
2020-10-13 10:38:34 -0700

    created signup/login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
+
     def new
     end
 
     def create
     end
-    
+
 end
