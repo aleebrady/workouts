d8b1a5b1b19ed828889be1ea05c99d3061f4854e
aleebrady <aleebrady@gmail.com>
2020-10-16 08:36:04 -0700

    added views for trainers

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,3 +1,11 @@
 class TrainersController < ApplicationController
+
+    def index
+        @trainers = Trainer.all
+    end
+
+    def show
+        @trainer = Trainer.find_by(id: params[:id])
+    end
   
 end
